5f26514e49fa55d7578c627e4023d0bdf5806bd7
Lucas Borges da Silva <lucasborges2812@gmail.com>
2026-02-03 03:08:29 +0000

    feat: add new tests on category spec

--- src/__tests__/category.entity.spec.ts
+++ src/__tests__/category.entity.spec.ts
@@ -6,20 +6,95 @@ import { Category } from "../domain/category.entity";
   // Assert - Verifico o resultado da ação
 
 describe('Category Unit Tests', () => {
-  test('should create a category with default values', () => {
-    //Arrange
-    const categoryProps = {
-      name: 'Movie'
-    }
+  test('should change name', () => {
+    const category = Category.create({
+      name: "Movie"
+    });
 
-    //Act
-    const category = new Category(categoryProps);
+    category.changeName("Movie att");
+    expect(category.name).toBe("Movie att");
+  });
 
-    //Assert
-    expect(category.category_id).toBeUndefined();
-    expect(category.name).toBe('Movie');
-    expect(category.description).toBeNull();
-    expect(category.is_active).toBeTruthy();
-    expect(category.created_at).toBeInstanceOf(Date);
-  })
+  test('should change description', () => {
+    const category = Category.create({
+      name: "Movie",
+      description: "some description"
+    });
+
+    category.changeDescription("other description");
+    expect(category.description).toBe("other description");
+  });
+
+  test('should active category', () => {
+    const category = Category.create({
+      name: "Movie",
+      description: "some description",
+      is_active: false
+    });
+
+    category.activate();
+    expect(category.is_active).toBe(true);
+  });
+
+  test('should deactive category', () => {
+    const category = Category.create({
+      name: "Movie",
+      description: "some description",
+      is_active: true
+    });
+
+    category.deactivate();
+    expect(category.is_active).toBe(false);
+  });
+
+  describe('constructor', () => {
+    test('should create a category with default values', () => {
+      //Arrange
+      const categoryProps = {
+        name: 'Movie'
+      }
+
+      //Act
+      const category = new Category(categoryProps);
+
+      //Assert
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe('Movie');
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBeTruthy();
+      expect(category.created_at).toBeInstanceOf(Date);
+    })
+  });
+
+  describe('create command', () => {
+    test('should create a category', () => {
+      //Arrange
+      const categoryProps = {
+        name: 'Movie'
+      }
+
+      //Act
+      const category = Category.create(categoryProps);
+
+      //Assert
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe('Movie');
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBeTruthy();
+      expect(category.created_at).toBeInstanceOf(Date);
+    })
+
+    test('should create a category with all properties', () => {
+      const category = Category.create({
+        name: 'Movie',
+        description: 'some description',
+        is_active: false
+      });
+
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe('Movie');
+      expect(category.description).toBe('some description');
+      expect(category.is_active).toBeFalsy();
+    })
+  });
 });
