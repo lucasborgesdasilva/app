e7480ecf2667d098e77d583692c4ee522da3648d
Lucas Borges da Silva <lucasborges2812@gmail.com>
2026-02-11 19:43:07 +0000

    feat: add unit tests for SearchParams and SearchResult

--- src/shared/domain/repository/repository-interface.ts
+++ src/shared/domain/repository/repository-interface.ts
@@ -20,6 +20,11 @@ export interface IRepository<E extends Entity, EntityId extends ValueObject> {
   getEntity(): new (...args: any[]) => E;
 }
 
+/**
+ * O ISearchableRepository é uma extensão do IRepository que adiciona a funcionalidade de busca com paginação, ordenação e filtragem.
+ * Ele define um método search que recebe os parâmetros de busca e retorna um resultado de busca paginado.
+ * Cada entidade específica pode ter seu próprio repositório que estende esse repositório genérico de busca.
+ */
 export interface ISearchableRepository<
   E extends Entity, 
   EntityId extends ValueObject, 
